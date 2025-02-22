08d40717dc0991fba84c81df8ddd1acfe2700bb6
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-02-22 16:56:13 -0500

    yer

--- tradertitan/tradertitan/settings.py
+++ tradertitan/tradertitan/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'game',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'tradertitan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["game/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
