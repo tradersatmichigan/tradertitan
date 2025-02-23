e50cc3220780a6c3577193226a5ca8fb345c2988
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-02-23 10:17:57 -0500

    user info

--- tradertitan/game/models.py
+++ tradertitan/game/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    positionSum = models.IntegerField()
+    totalPnl = models.IntegerField()
+
