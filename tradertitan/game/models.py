c8f12ecb706f12949c39134ca1e431029e9b4ce7
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-02-23 13:29:23 -0500

    room info

--- tradertitan/game/models.py
+++ tradertitan/game/models.py
@@ -3,6 +3,20 @@ from django.contrib.auth.models import User
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    positionSum = models.IntegerField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    totalRank = models.IntegerField()
     totalPnl = models.IntegerField()
 
+
+class Quote(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    price = models.IntegerField()
+
+class Room(models.Model):
+    bid = Quote()
+    ask = Quote()
+
+class Round(models.Model):
+    market = models.CharField() # asset begin traded
+    trueValue = models.IntegerField()
