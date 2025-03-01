19435d1f91b697079f92ef6a6e3bf0783639f844
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-01 12:54:50 -0500

    structure

--- main.go
+++ main.go
@@ -1,25 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-  if r.Method == http.MethodGet {
-    http.ServeFile(w, r, "join.html")
-  } else if r.Method == http.MethodPost {
-    if r.ParseForm() != nil {
-      log.Fatal("Form parse fail")
-    }
-    fmt.Fprintf(w, "%s", r.Form.Get("username"))
-  } else {
-    http.Error(w, "Bad Request", http.StatusBadRequest)
-  }
-}
+import "fmt"
 
 func main() {
-  http.HandleFunc("/join/", loginHandler)
-  log.Fatal(http.ListenAndServe(":8080", nil))
+  fmt.Println("Run")
 }
