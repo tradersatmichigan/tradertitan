2abe50dde90e3bded597fa1e6d13300d78a9b504
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-11 16:02:34 -0400

    handle

--- game/api.go
+++ game/api.go
@@ -34,9 +34,43 @@ func HandleLogin(w http.ResponseWriter, r *http.Request) {
   }
 }
 
-func ServeGame(w http.ResponseWriter, r *http.Request, _ string) {
+func ServeGame(w http.ResponseWriter, r *http.Request, _ string) { // also return current state
   if r.Method == http.MethodGet {
     http.ServeFile(w, r, "html/game.html")
+  } else {
+    http.Error(w, "Bad request method", http.StatusBadRequest)
+  }
+}
+
+func GetStream(w http.ResponseWriter, r *http.Request, username string) {
+  if r.Method == http.MethodGet {
+
+  } else {
+    http.Error(w, "Bad request method", http.StatusBadRequest)
+  }
+}
+
+func PostWidth(w http.ResponseWriter, r *http.Request, username string) {
+  if r.Method == http.MethodPost {
+
+  } else {
+    http.Error(w, "Bad request method", http.StatusBadRequest)
+  }
+}
+
+func PostCenter(w http.ResponseWriter, r *http.Request, username string) {
+  if r.Method == http.MethodPost {
+    if r.ParseForm() == nil {
+
+    }
+  } else {
+    http.Error(w, "Bad request method", http.StatusBadRequest)
+  }
+}
+
+func PostTrade(w http.ResponseWriter, r *http.Request, username string) {
+  if r.Method == http.MethodPost {
+
   } else {
     http.Error(w, "Bad request method", http.StatusBadRequest)
   }
