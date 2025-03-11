293a4bbe9e6478323a878f11726c543358f9f7b2
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-10 23:14:50 -0400

    mutex

---
+++ game/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"math"
+	"math/big"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var gameId string
+
+func handleJoin(w http.ResponseWriter, r *http.Request) {
+  if r.Method == http.MethodGet {
+    cookie, err := r.Cookie("gameId")
+
+    if err == nil && cookie.Value == gameId {
+      fmt.Fprintf(w, "Already signed in, please continue to the game")
+    } else {
+      http.ServeFile(w, r, "html/join.html")
+    }
+
+  } else if r.Method == http.MethodPost {
+
+    if err := r.ParseForm(); err != nil {
+			http.Error(w, "Error parsing form", http.StatusBadRequest)
+			return
+		}
+
+		username := r.FormValue("username")
+		if username == "" {
+			http.Error(w, "Username is required", http.StatusBadRequest)
+			return
+		}
+
+    mtx.Lock()
+    if _, ok := users[username]; ok {
+      http.Error(w, "Name already in use", http.StatusBadRequest)
+    } else {
+      users[username] = User{}
+
+      http.SetCookie(w, &http.Cookie{
+        Name: "gameId",
+        Value: gameId,
+        Expires: time.Now().Add(3 * time.Hour),
+        HttpOnly: true,
+        Path: "/",
+      })
+
+      http.SetCookie(w, &http.Cookie{
+        Name: "username",
+        Value: username,
+        Expires: time.Now().Add(3 * time.Hour),
+        HttpOnly: true,
+        Path: "/",
+      })
+
+      http.Redirect(w, r, "/", http.StatusOK)
+    }
+    mtx.Unlock()
+
+  } else {
+    http.Error(w, "Bad request method", http.StatusBadRequest)
+  }
+}
+
+func main() {
+  port := flag.String("port", "8080", "port to listen on")
+  input := flag.String("file", "test.txt", "file path to load rounds from")
+
+  flag.Parse()
+  
+  rounds := getRounds(*input)
+  fmt.Println(rounds) // for compile
+
+  server := &http.Server{Addr: ":" + *port}
+
+  http.HandleFunc("/join", handleJoin)
+
+  go func() {
+    fmt.Println("server started on ", *port)
+    if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("Server error: %v\n", err)
+		}
+  }()
+
+  // add view advances here
+  fmt.Println("Kill server?")
+  waitForEnter() 
+
+  ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+  defer cancel()
+
+  if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Server forced to shutdown: %v\n", err)
+	} else {
+		fmt.Println("Server gracefully stopped.")
+	}
+}
+
+func init() {
+  // create a random number to verify login
+  random, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
+  if err != nil {
+    panic(err)
+  }
+  gameId = random.String()
+}
+
+func getRounds(filepath string) []Round {
+  file, err := os.Open(filepath)
+  if err != nil {
+    panic(err)
+  }
+  defer file.Close()
+
+  var Rounds []Round
+
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    line := scanner.Text()
+    parts := strings.SplitN(line, " ", 2)
+
+    number, err := strconv.Atoi(parts[0])
+    if err != nil {
+      panic(err)
+    }
+
+    Rounds = append(Rounds, Round{parts[1], number})
+  }
+
+  return Rounds
+}
+
+func waitForEnter() {
+  bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
--- game/types.go
+++ game/types.go
@@ -2,51 +2,11 @@ package main
 
 type Username = string
 
-type RegisterResult = int
-const (
-  Success RegisterResult = iota
-  UsernameTaken
-)
-
 type User struct {
-  Room uint
-  TotalPlacement uint
-  TotalPnl int
-
-  Side Side
-  CurPnl int
+  room uint
 }
 
 type Round struct {
   Market string
-  TrueVal int
+  TrueValue int
 }
-
-type Quote struct {
-  Username Username
-  Center   int
-  Width    uint
-}
-
-type Side = int
-const (
-  Long Side = iota
-  Short
-  None
-)
-
-type SpreadArgs struct {
-  Username Username
-  Width uint
-}
-
-type CenterArgs struct {
-  Username Username
-  Center int
-}
-
-type TradeArgs struct {
-  Username Username
-  Side Side
-}
-
