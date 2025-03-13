9fad566cf8a8cc8df8d2c09e714fe3e9b3c86ea7
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-13 15:40:30 -0400

    good

--- game/api.go
+++ game/api.go
@@ -76,8 +76,6 @@ func GetStream(w http.ResponseWriter, r *http.Request, username string) {
             return
           }
 
-          fmt.Println("state: ", state, ", data: ", data)
-
           fmt.Fprintf(w, "data: %s\n\n", data)
           flusher.Flush()
         }
